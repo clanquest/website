eb2a8b621fd5a745e7e79d53f51fd6c61da005ed
Shane <shane12088@gmail.com>
2020-06-09 18:15:14 -0600

    Join brochure words

--- static/join_home.php
+++ static/join_home.php
@@ -1,64 +1,65 @@
 <div class="home-brochure">
-	<p>Clan Quest is a clan representing every nationality that plays video games. 
-	All of us are united by our love of quests and lore. We complete every quest, 
-	examine every piece of lore, and love doing it. We invite anyone, from novice 
-	to expert, to join our community.</p>
+	<p>Clan Quest is an online community, originally created for players who enjoy 
+	the lore and quests of RuneScape, and has since branched out with members in 
+	over 30 countries. It organizes its members into <a href="https://clanquest.org/wiki/Guilds" target="_blank">Guilds</a>, centered around different 
+	games and interests, that comprise <a href="https://clanquest.org/wiki/Clan_Quest" target="_blank">Clan Quest</a> as a whole. Clan Quest members 
+	enjoy questing and lore, providing help with difficult aspects of games, 
+	and making terrible puns.</p>
 
-	<h2>20+ Countries</h2>
+	<!-- <div id="guild-images">
+		<img src="/forums/styles/clanquest/images/join_brochure/OS_Guild.jpg" srcset="/forums/styles/clanquest/images/join_brochure/OS_Guild_2x.jpg 2x" alt="Old School RuneScape Guild"> 
+		<img src="/forums/styles/clanquest/images/join_brochure/RS_Guild.jpg" srcset="/forums/styles/clanquest/images/join_brochure/RS_Guild_2x.jpg 2x" alt="RuneScape Guild"> 
+		<img src="/forums/styles/clanquest/images/join_brochure/RL_Guild.jpg" srcset="/forums/styles/clanquest/images/join_brochure/RL_Guild_2x.jpg 2x" alt="Rocket League Guild">
+	</div> -->
+	
+	<img src="https://clanquest.org/wiki/images/7/7f/Logo_Center.png" width="300">
 
-	<img src="/forums/styles/clanquest/images/join_brochure/our_countries.png" srcset="/forums/styles/clanquest/images/join_brochure/our_countries_2x.png 2x" alt="Countries Represented By Clan Quest">
+	<p>Since 2009 the "Quest Noobs" of Clan Quest have played RuneScape. Its members 
+	are frequently recognized by Jagex, have had many pieces of suggested content added 
+	in game and have had multiple Jagex employees in our community. Clan Quest unites 
+	people who are passionate about RuneScape, its lore, and writing the guides others read.</p>
 
-	<p>Whether you’re in the prairies of Western Canada, the Dutch countryside, the 
-	south island of New Zealand or anywhere in between you’ll always find someone 
-	online to share your gaming session with.</p>
+	<img src="https://clanquest.org/wiki/images/7/7f/Logo_Center.png" width="300">
 
-	<h2>Our Guilds</h2>
+	<p>The Old School RuneScape (OSRS) Guild is composed of players who prefer a more classic 
+	Runescape experience. This Guild is for those questers who enjoy Dragon Slayer 2, Monkey Madness 2, 
+	and all the quests again in a 2007 style package. If you like the “original version” of RuneScape 
+	check out the OSRS Guild, where its members are almost as opinionated as awesome.</p>
 
-	<img src="/forums/styles/clanquest/images/join_brochure/rs3_citadel.jpg" srcset="/forums/styles/clanquest/images/join_brochure/rs3_citadel_2x.jpg 2x" alt="The RuneScape 3 Clan Citadel">
+	<img src="https://clanquest.org/wiki/images/7/7f/Logo_Center.png" width="300">
+	<p>Members of our Rocket League Guild love playing soccer with their cars. Clan Quest isn’t all 
+	about lore, we love our fast paced games too! They play both casual and competitive and are always 
+	looking for new members. All we are going to say is, “GGOOOOAAAALLLLLL!!!”</p>
 
-	<p>Since 2009 the &quot;Quest Noobs&quot; of Clan Quest have been a dominant 
-	force within the game of RuneScape. The Quest Noobs are frequently recognized by 
-	Jagex, including members' content appearing in game and having multiple JMods join 
-	our community (both officially and unofficially). Clan Quest unites those people 
-	who are passionate about RuneScape's lore and who love engaging with a 
-	community that holds each other up.</p>
+	<img src="https://clanquest.org/wiki/images/7/7f/Logo_Center.png" width="300">
+	<p>Like Clan Quest? Like RuneScape? Are you an Ironman? Then the RuneScape 3 Ironman Guild is 
+	for you. The Guild is tailored specifically for players who play the Ironman mode including group 
+	PvM and Questing.</p>
 
-	<p>We take our relationship to our clan members very seriously. We believe strongly that real 
-	life commitments are more important than commitment to the clan. Any member is allowed to 
-	join any event or any communication channel we provide. With this we don’t have any 
-	activity requirements to retain membership.</p>
+	<h2>Our Meetups</h2>
 
-	<img src="/forums/styles/clanquest/images/join_brochure/osrs_quest.jpg" alt="Questing in Old School RuneScape">
+	<img src="/forums/styles/clanquest/images/join_brochure/cq_irl.jpg" srcset="/forums/styles/clanquest/images/join_brochure/cq_irl_2x.jpg 2x" alt="Our Meetups in Person">
 
-	<p>A relatively new addition to Clan Quest's organization, the Old School RuneScape guild 
-	is comprised of many players who originally discovered our community through its involvement 
-	in RuneScape 3. With time, the guild has developed as its own entity, and its membership is unique 
-	and growing. If you have a love for the more retro of Jagex's MMOs, check out the OSRS Guild!</p>
+	<p>While Clan Questers obviously play video games together, and use our forums and <a href="https://discord.clanquest.org" target="_blank">Discord server</a> to 
+	communicate, real life meetups have become one of our community’s coolest activities. With our 
+	continued growth, we have hosted meetings on four continents, and continue to aspire to be 
+	a leader in the online community.</p>
 
-	<p>Want to become a noob? <a href="https://clanquest.org/forums/viewforum.php?f=2">Become a member!</a></p>
+	<p>Whether it’s a small meetup in Atlanta, attending a game convention like RuneFest, or 
+	larger gatherings like the clan’s own Quest Fest, meetups have become regular occurrences. 
+	Friends from online are becoming friends in person, and while not mandatory, many of us 
+	have found meetups are a joy to attend. Friendships made in Clan Quest can expand to the real world if you desire.</p>
 
-	<h2>Real World</h2>
+	<img src="https://clanquest.org/wiki/images/1/11/Member_Countries_Map.png" alt="30+ Countries" width="700">
 
-	<img src="/forums/styles/clanquest/images/join_brochure/runefest18.jpg" srcset="/forums/styles/clanquest/images/join_brochure/runefest18_2x.jpg 2x" alt="Clan Quest at RuneFest 2018">
+	<h2>Join Us</h2>
 
-	<p>Clan Questers spend a lot of hours playing video games together and chatting on our 
-	forums and <a href="https://discord.gg/ejWecQP">Discord server</a>. As such some have even elected to meet other Clan Questers in real life!</p>
+	<p>If you’re someone who loves the story, lore, and quests of Runescape, or if you find yourself 
+	interested in any of our guilds, then join Clan Quest today! We’re a laid back group who focuses 
+	on our community first and foremost. We invite anyone from novice to expert to join us for their next adventure.</p>
 
-	<p>Whether it’s a small meetup in Atlanta or attending RuneFest, Clan Quest has regular meetups. 
-	Meetups are of course not mandatory but they are a joy to attend. Friendships made in Clan Quest true 
-	and authentic and have potential to expand to the real world if you desire.</p>
+	<p>The adventure continues... <a href="https://clanquest.org/forums/viewforum.php?f=2">Will you be our next member?</a></p>
 
-	<p>Clan Quest has also attended RuneFest in force. For consecutive years Clan Quest 
-	has had 15 or more clan members attending from all over the world! Going to 
-	RuneFest? We’ll see you there!!!</p>
-
-	<h2>Become a Member</h2>
-
-	<p>Clan Quest is the ultimate gaming clan for those who love story, lore, and quests. 
-	We’re a laid back group who focuses on our community first and foremost. We invite anyone 
-	from novice to expert to join us for their next adventure.</p>
-
-	<p>The adventure continues... <a href="https://clanquest.org/forums/viewforum.php?f=2">Will you be the next noob?</a></p>
 	<img src="/forums/styles/clanquest/images/cq_shield.png" srcset="/forums/styles/clanquest/images/cq_shield_2x.png 2x" alt="Clan Quest">
 
 </div>
