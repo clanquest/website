e78107467531371c859d6db36c804f2f9bc91a67
Shane <shane12088@gmail.com>
2020-08-04 15:37:39 -0600

    Anchor click padding

--- forumtheme/template/layout.js
+++ forumtheme/template/layout.js
@@ -2,4 +2,18 @@ $(document).ready(function() {
 	$('#cq-navbar-toggle').click(function() {
 		$('#cq-navbar').toggleClass('open');
 	});
+
+	function offsetAnchorClick() {
+		if (location.hash.length !== 0)
+			window.scrollTo(window.scrollX, window.scrollY - 50);
+	}
+
+	$(document).on('click', 'a[href^="#"]', function(evt) {
+		// use click because this is caught before hashchange
+		window.setTimeout(function() {
+			offsetAnchorClick();
+		}, 0);
+	});
+
+	window.setTimeout(offsetAnchorClick, 0);
 });
