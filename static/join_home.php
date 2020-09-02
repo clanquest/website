9e5853ec49be5901bb82b1a8704ff638a32a3150
Shane <shane12088@gmail.com>
2020-09-02 16:21:52 -0600

    Alt text and titles

--- static/join_home.php
+++ static/join_home.php
@@ -8,7 +8,7 @@
 	
 	<div id="our-guilds">
 		<div id="rs3">
-		<img src="/forums/styles/clanquest/images/join_brochure/rs3-text-banner.png" height="100" alt="RuneScape 3">
+		<img src="/forums/styles/clanquest/images/join_brochure/rs3-text-banner.png" height="100" title="RuneScape 3" alt="RuneScape 3 Guild">
 		<p>Since 2009 the "Quest Noobs" of Clan Quest have played RuneScape. Its members 
 		are frequently recognized by Jagex, have had many pieces of suggested content added 
 		in game and have had multiple Jagex employees in our community. Clan Quest unites 
@@ -16,7 +16,7 @@
 		</div>
 
 		<div id="os">
-		<img src="/forums/styles/clanquest/images/join_brochure/os-text-banner.png" height="100" alt="Old School">
+		<img src="/forums/styles/clanquest/images/join_brochure/os-text-banner.png" height="100" title="Old School" alt="Old School Guild">
 		<p>The Old School RuneScape (OSRS) Guild is composed of players who prefer a more classic 
 		Runescape experience. This Guild is for those questers who enjoy Dragon Slayer 2, Monkey Madness 2, 
 		and all the quests again in a 2007 style package. If you like the “original version” of RuneScape 
@@ -24,14 +24,14 @@
 		</div>
 
 		<div id="rl">
-		<img src="/forums/styles/clanquest/images/join_brochure/rl-text-banner.png" height="100" alt="Rocket League">
+		<img src="/forums/styles/clanquest/images/join_brochure/rl-text-banner.png" height="100" title="Rocket League" alt="Rocket League Guild">
 		<p>Members of our Rocket League Guild love playing soccer with their cars. Clan Quest isn’t all 
 		about lore, we love our fast paced games too! They play both casual and competitive and are always 
 		looking for new members. All we are going to say is, “GGOOOOAAAALLLLLL!!!”</p>
 		</div>
 
 		<div id="im">
-		<img src="/forums/styles/clanquest/images/join_brochure/im-text-banner.png" height="100" alt="Ironman RuneScape 3">
+		<img src="/forums/styles/clanquest/images/join_brochure/im-text-banner.png" height="100" title="Ironman RuneScape 3" alt="Ironman RuneScape 3 Guild">
 		<p>Like Clan Quest? Like RuneScape? Are you an Ironman? Then the RuneScape 3 Ironman Guild is 
 		for you. The Guild is tailored specifically for players who play the Ironman mode including group 
 		PvM and Questing.</p>
@@ -39,7 +39,7 @@
 	</div>
 	<h2>Our Meetups</h2>
 
-	<img src="/forums/styles/clanquest/images/join_brochure/cq_irl.jpg" srcset="/forums/styles/clanquest/images/join_brochure/cq_irl_2x.jpg 2x" alt="Our Meetups in Person">
+	<img src="/forums/styles/clanquest/images/join_brochure/cq_irl.jpg" srcset="/forums/styles/clanquest/images/join_brochure/cq_irl_2x.jpg 2x" title="Our Meetups in Person" alt="A collage of our meetups in person">
 
 	<p>While Clan Questers obviously play video games together, and use our forums and <a href="https://discord.clanquest.org" target="_blank">Discord server</a> to 
 	communicate, real life meetups have become one of our community’s coolest activities. With our 
@@ -51,7 +51,7 @@
 	Friends from online are becoming friends in person, and while not mandatory, many of us 
 	have found meetups are a joy to attend. Friendships made in Clan Quest can expand to the real world if you desire.</p>
 
-	<img src="https://clanquest.org/wiki/images/1/11/Member_Countries_Map.png" alt="30+ Countries" width="700">
+	<img src="https://clanquest.org/wiki/images/1/11/Member_Countries_Map.png" title="30+ Countries" alt="Clan Quest has members in 30+ countries" width="700">
 
 	<h2>Join Us</h2>
 
@@ -61,6 +61,6 @@
 
 	<p>The adventure continues... <a href="https://clanquest.org/forums/viewforum.php?f=2">Will you be our next member?</a></p>
 
-	<img src="/forums/styles/clanquest/images/cq_shield.png" srcset="/forums/styles/clanquest/images/cq_shield_2x.png 2x" alt="Clan Quest">
+	<img src="/forums/styles/clanquest/images/cq_shield.png" srcset="/forums/styles/clanquest/images/cq_shield_2x.png 2x" title="Clan Quest" alt="Clan Quest Shield Logo">
 
 </article>
