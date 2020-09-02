d414b1c674204b48b8d9737d1284cbbf1f3621e0
Shane <shane12088@gmail.com>
2020-09-01 21:29:47 -0600

    Homepage sidebar

--- dynamic/members_home.php
+++ dynamic/members_home.php
@@ -170,15 +170,16 @@
 	?>
 	</div>
 	<aside class="side-widgets cq-info-box">
+		<h2>Help Wanted</h2>
+		<p>Clan Quest is always looking for those interested in helping out within our community. Our community is built by our members.</p>
+		<p>Want to help us out? Check our <a href="https://clanquest.org/wiki/Help_Wanted">Help Wanted</a> page.</p>
 		<h2>Upcoming Events</h2>
 		<iframe
 			src="https://calendar.google.com/calendar/embed?title=Clan%20Quest%20Events&amp;showTitle=0&amp;showNav=0&amp;showTabs=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=clanquest.org_47b3e6k7791rj8al8mr18iujm0%40group.calendar.google.com&amp;color=%235F6B02&amp;src=clanquest.org_egecc810jrhbl9p7au75rvd49c%40group.calendar.google.com&amp;color=%2342104A&amp;src=clanquest.org_ecai4tp12r30p792i7tghbghos%40group.calendar.google.com&amp;color=%2328754E&amp;src=clanquest.org_iikv32sksnohap5iscjut6fi98%40group.calendar.google.com&amp;color=%23B1440E&amp;src=clanquest.org_cnjh5q7ci6fqic3tnap4r7asr4%40group.calendar.google.com&amp;color=%23711616&amp;src=clanquest.org_s9393s458a7ffaps1deoft1pck%40group.calendar.google.com&amp;color=%23B1440E&amp;src=clanquest.org_bu3rhro5uufh6amfg5fd23nof8%40group.calendar.google.com&amp;color=%23711616&amp;ctz=UTC"
 			frameborder="0"
 			scrolling="no"
 			class="google-calendar-embed"></iframe>
-		<?php if(!$user->data['user_new']): ?>
-			<h2>Discord</h2>
-			<iframe src="https://discordapp.com/widget?id=132427319246585856&theme=dark" allowtransparency="true" frameborder="0" class="discord-widget"></iframe>
-		<?php endif; ?>
+		<h2>Social Media</h2>
+		<a class="twitter-timeline" data-width="350" data-height="400" data-theme="light" href="https://twitter.com/ClanQuest?ref_src=twsrc%5Etfw">&nbsp;</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 	</aside>
 </div>
