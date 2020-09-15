7e22cd933274b35cf5cfc8032f0849ae5859f967
Shane <shane12088@gmail.com>
2020-09-15 14:36:48 -0600

    Remove help wanted temporarily

--- dynamic/members_home.php
+++ dynamic/members_home.php
@@ -170,9 +170,6 @@
 	?>
 	</div>
 	<aside class="side-widgets cq-info-box">
-		<h2>Help Wanted</h2>
-		<p>Clan Quest is always looking for those interested in helping out within our community. Our community is built by our members.</p>
-		<p>Want to help us out? Check our <a href="https://clanquest.org/wiki/Help_Wanted">Help Wanted</a> page.</p>
 		<h2>Upcoming Events</h2>
 		<iframe
 			src="https://calendar.google.com/calendar/embed?title=Clan%20Quest%20Events&amp;showTitle=0&amp;showNav=0&amp;showTabs=0&amp;showPrint=0&amp;showCalendars=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=clanquest.org_47b3e6k7791rj8al8mr18iujm0%40group.calendar.google.com&amp;color=%235F6B02&amp;src=clanquest.org_egecc810jrhbl9p7au75rvd49c%40group.calendar.google.com&amp;color=%2342104A&amp;src=clanquest.org_ecai4tp12r30p792i7tghbghos%40group.calendar.google.com&amp;color=%2328754E&amp;src=clanquest.org_iikv32sksnohap5iscjut6fi98%40group.calendar.google.com&amp;color=%23B1440E&amp;src=clanquest.org_cnjh5q7ci6fqic3tnap4r7asr4%40group.calendar.google.com&amp;color=%23711616&amp;src=clanquest.org_s9393s458a7ffaps1deoft1pck%40group.calendar.google.com&amp;color=%23B1440E&amp;src=clanquest.org_bu3rhro5uufh6amfg5fd23nof8%40group.calendar.google.com&amp;color=%23711616&amp;ctz=UTC"
