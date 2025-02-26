ccb0ca11be5f1ca77a1b8557d4b1aef585a20b9e
Shane <shane12088@gmail.com>
2025-02-25 21:31:51 -0700

    Join page language cleanup

--- static/join_home.php
+++ static/join_home.php
@@ -9,10 +9,10 @@
 	<div id="our-guilds">
 		<div id="rs3">
 		<a href="https://clanquest.org/wiki/Questing_Clan_of_RuneScape"><img src="/forums/styles/clanquest/images/join_brochure/rs3-icon.png" height="100" title="RuneScape 3" alt="RuneScape 3 Guild"></a>
-		<p>Since 2009 the "Quest Noobs" of Clan Quest have played RuneScape. Its members 
-		are frequently recognized by Jagex, have had many pieces of suggested content added 
-		in game and have had multiple Jagex employees in our community. Clan Quest unites 
-		people who are passionate about RuneScape, its lore, and writing the guides others read.</p>
+		<p>Since 2009, the "Quest Noobs" of Clan Quest have played RuneScape. Its members are 
+			recognized by Jagex, appreciate the uniqueness of RuneScape, have had content added 
+			to the game, and have had multiple Jagex employees in our community. Clan Quest unites 
+			people who are passionate about RuneScape and the game's quests and lore.</p>
 		</div>
 
 		<div id="os">
@@ -34,23 +34,19 @@
 
 	<img src="/forums/styles/clanquest/images/join_brochure/cq_irl.jpg" srcset="/forums/styles/clanquest/images/join_brochure/cq_irl_2x.jpg 2x" title="Our Meetups in Person" alt="A collage of our meetups in person">
 
-	<p>While Clan Questers obviously play video games together, and use our forums and <a href="https://discord.clanquest.org" target="_blank">Discord server</a> to 
-	communicate, real life meetups have become one of our community’s coolest activities. With our 
+	<p>While Clan Questers obviously play video games together and use our forums and <a href="https://discord.clanquest.org" target="_blank">Discord server</a> to 
+	communicate, real life meetups have become one of our community’s coolest activities. Whether it’s a small meetup in Atlanta, attending a game convention like RuneFest, or 
+	larger gatherings like the clan’s own Quest Fest, meetups extend our community to the real world. With our 
 	continued growth, we have hosted meetings on four continents, and continue to aspire to be 
 	a leader in the online community.</p>
-
-	<p>Whether it’s a small meetup in Atlanta, attending a game convention like RuneFest, or 
-	larger gatherings like the clan’s own Quest Fest, meetups have become regular occurrences. 
-	Friends from online are becoming friends in person, and while not mandatory, many of us 
-	have found meetups are a joy to attend. Friendships made in Clan Quest can expand to the real world if you desire.</p>
 
 	<img src="https://clanquest.org/wiki/images/1/11/Member_Countries_Map.png" title="30+ Countries" alt="Clan Quest has members in 30+ countries" width="700">
 
 	<h2>Join Us</h2>
 
-	<p>If you’re someone who loves the story, lore, and quests of Runescape, or if you find yourself 
-	interested in any of our guilds, then join Clan Quest today! We’re a laid back group who focuses 
-	on our community first and foremost. We invite anyone from novice to expert to join us for their next adventure.</p>
+	<p>Whether you’re someone who loves the lore and quests of RuneScape, if you just love RuneScape, or find yourself interested in any of our other Guilds then join Clan Quest today! 
+		We’re a laid back group who focuses on our community first and foremost. If you want to say hello, join our <a href="https://discord.clanquest.org" target="_blank">Discord</a>. 
+		We invite anyone from novice to expert to join us for their next adventure.</p>
 
 	<p>The adventure continues...<br><a href="https://apply.clanquest.org/rs3">Join RS3</a> | <a href="https://apply.clanquest.org/os">Join OSRS</a> | <a href="https://clanquest.org/forums/viewtopic.php?t=7608">Join Chess</a></p>
 
